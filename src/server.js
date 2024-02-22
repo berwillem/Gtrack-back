69f68afd4dd90a504b2b9f6ae556d62ccd56d32e
berchiche willem <berwillempro@gmail.com>
2024-02-22 01:21:45 +0100

    first-commit

---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const coockieParser = require("cookie-parser");
+//require .env
+require("dotenv").config();
+//db config
+const connectDB = require("./config/db");
+
+//init server
+const server = express();
+
+// cookie
+server.use(coockieParser());
+
+//init cors
+server.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+//use json
+server.use(express.json());
+
+//require routes
+
+// const routes = require("./router");
+// server.use(routes);
+
+//run server
+connectDB()
+  .then((con) => {
+    console.log(
+      `database connected host:${con.connection.host},dbname:${con.connection.db.databaseName}`
+    );
+    handleEventListeners();
+
+    server.listen(process.env.PORT, () => {
+      console.log(
+        `express server running on port ${process.env.PORT} in ${server.get(
+          "env"
+        )}`
+      );
+    });
+  })
+  .catch((err) => console.error("failed to connect to database"));
+
+module.exports = server;
